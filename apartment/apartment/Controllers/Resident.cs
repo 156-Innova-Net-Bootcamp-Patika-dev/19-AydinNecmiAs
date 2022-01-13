773330eba319569adc45678d1556707a70ba6991
Aydın Necmi AS <49839084+aydinnecmias@users.noreply.github.com>
2022-01-13 19:53:59 +0300

    Week-1 Homework Web API
    Apartment Management API

---
+++ apartment/apartment/Controllers/Resident.cs
@@ -0,0 +1,21 @@
+﻿namespace apartment.Controllers
+{
+    public class Resident
+    {
+        public int Id { get; set; }
+        public string ResidentName { get; set; }
+        public string ResidentPhone { get; set; }
+
+        public string ApartmentNumber { get; set; }
+
+        public string FloorNumber { get; set; }
+
+        public bool RentStatus { get; set; }
+
+        public string Type { get; set; }
+
+        public bool DuesStatus { get; set; }
+
+        public string Bills { get; set; }
+    }
+}
